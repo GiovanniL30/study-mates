06227a9291b56b119f7f60a072403d6b99aa2d74
Marshall Cyrille Marcelo <mcam49856@gmail.com>
2025-12-11 20:35:41 +0800

    clarity tracker for heatmap

--- src/components/layout/CourseLayout.tsx
+++ src/components/layout/CourseLayout.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Header from "@/components/ui/Header";
+import ClarityRouteTracker from "@/lib/ClarityRouteTracker";
 
 const CourseLayout = () => {
   return (
     <main className="flex flex-col gap-2 bg-background w-full min-h-screen p-4">
+      <ClarityRouteTracker />
       <Header />
       <div className="flex justify-center w-full">
         <Outlet />
